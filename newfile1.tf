793626bea9b2958475d266912d4f4c452ace3f0e
sbirudar <52947502+sbirudar@users.noreply.github.com>
2019-09-03 20:26:53 +0530

    Update and rename newfile1 to newfile1.tf

---
+++ newfile1.tf
@@ -0,0 +1,17 @@
+provider "azurerm" {
+    subscription_id = "d608cbc8-550f-48a0-8448-d73acfa4c6c1"
+    client_id       = "7c7fefcb-2a38-447f-addb-0f4fc6a0f398"
+    client_secret   = "n/c5BA=xwnDd]Q-bqsYKU5oRPZNXk1R0"
+    tenant_id       = "fc8904d3-a019-4325-a31f-5507517cce51"
+}
+
+
+
+resource "azurerm_resource_group" "myterraformgroup" {
+    name     = "myResourceGroup"
+    location = "eastus"
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
